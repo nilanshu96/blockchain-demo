1cc5c0db5a13e1f87e39e2f2f9c8ebf78c21b29f
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-04-16 16:10:43 +0530

    added css and html for block element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import Block from "./components/block/Block";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Block />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/block/Block.js
@@ -0,0 +1,13 @@
+const Block = () => {
+  return (
+    <div className="block w-500 box-1 padding-5">
+      <div className=" box-1 flex w-100p">
+        <div className="br-1 center pa-v-2 pa-h-5 inline-block">DATA</div>
+
+        <input placeholder="Enter Data" className="f-m fg-1 pa-h-4"></input>
+      </div>
+    </div>
+  );
+};
+
+export default Block;
