d0a92501945b0a480a523379df8afd77c13ecc46
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-04-19 17:30:39 +0530

    added event listeners to add new block

--- src/components/add-data-button/AddDataButton.js
+++ src/components/add-data-button/AddDataButton.js
@@ -1,13 +1,42 @@
-const AddDataButton = () => {
+import { useState, useRef } from "react";
+
+const AddDataButton = ({ addNewBlock }) => {
+  const [data, setData] = useState("");
+
+  const onDataChange = (event) => setData(event.target.value);
+
+  const submitButton = useRef();
+
+  const onPressEnter = (event) => {
+    if (event.keyCode === 13) {
+      submitButton.current.click();
+    }
+  };
+
+  const onSubmitClick = () => {
+    setData("");
+    addNewBlock(data);
+  };
+
   return (
     <div className="box-1 box-hover br-2 padding-5 flex flex-column w-fit-content mv-5">
       <div className="box-1-gray flex">
         <div className="br-1-gray ta-center pa-v-2 pa-h-5 inline-block bg-lg">
           DATA
         </div>
-        <input placeholder="Enter Data" className="fg-4 pa-h-4"></input>
+        <input
+          placeholder="Enter Data"
+          className="fg-4 pa-h-4"
+          onChange={onDataChange}
+          value={data}
+          onKeyUp={onPressEnter}
+        ></input>
       </div>
-      <button className="padding-2 mt-5 w-fit-content center pa-h-5">
+      <button
+        className="padding-2 mt-5 w-fit-content center pa-h-5"
+        onClick={onSubmitClick}
+        ref={submitButton}
+      >
         ADD DATA
       </button>
     </div>
